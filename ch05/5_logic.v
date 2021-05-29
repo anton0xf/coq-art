4f474d7c943fde0d2e925dfb63ae46fa55641674
anton0xf <anton0xf@gmail.com>
2021-05-29 11:44:34 +0500

    ch05: more examples

--- ch05/5_logic.v
+++ ch05/5_logic.v
@@ -15,4 +15,94 @@ Proof. simpl. intro H. assumption. Qed.
 
 Print conv_example''.
 (* conv_example'' = fun (n : nat) (H : 35 < n) => H
-(*      : forall n : nat, 7 * 5 < n -> 6 * 6 <= n *)
+   : forall n : nat, 7 * 5 < n -> 6 * 6 <= n *)
+
+Definition neutral_left (A : Set) (op : A -> A -> A) (e : A) : Prop
+  := forall x : A, op e x = x.
+
+Theorem one_neutral_left : neutral_left Z Zmult 1%Z.
+Proof. intro z. apply Z.mul_1_l. Qed.
+
+Lemma le_i_SSi : forall i : nat, i <= S (S i).
+Proof. intro i. apply le_S, le_S, le_n. Qed.
+
+Lemma all_imp_dist : forall (A : Type) (P Q : A -> Prop),
+    (forall x : A, P x -> Q x)
+    -> (forall y : A, P y) -> forall z : A, Q z.
+Proof. intros A P Q H Hp z. apply H, Hp. Qed.
+
+Check le_trans.
+(* Nat.le_trans : forall n m p : nat, n <= m -> m <= p -> n <= p *)
+
+Check mult_le_compat_l.
+(* mult_le_compat_l : forall n m p : nat,
+                        n <= m -> p * n <= p * m *)
+
+Theorem mult_le_compat_r (m n p : nat) : n <= p -> n * m <= p * m.
+Proof.
+  intro H. rewrite (mult_comm n m). rewrite (mult_comm p m).
+  apply mult_le_compat_l. assumption.
+Qed.
+
+
+Theorem le_mult_mult (n m p q : nat)
+  : n <= m -> p <= q -> n * p <= m * q.
+Proof.
+  intros H1 H2.
+  (* apply le_trans with (m := (m * p)). *)
+  apply (le_trans _ (m * p)).
+  - apply mult_le_compat_r, H1.
+  - apply mult_le_compat_l, H2.
+Qed.
+
+Theorem le_mult_mult' (n m p q : nat)
+  : n <= m -> p <= q -> n * p <= m * q.
+Proof.
+  intros H1 H2. eapply le_trans.
+  - apply mult_le_compat_r. exact H1.
+  - apply mult_le_compat_l, H2.
+Qed.
+
+Lemma le_O_mult (n p : nat) : 0 * n <= 0 * p.
+Proof. apply le_n. Qed.
+
+Lemma le_O_mult_R (n p : nat) : n * 0 <= p * 0.
+Proof.
+  (* apply le_n. (* => Unable to unify "p * 0" with "n * 0". *) *)
+  repeat rewrite (mult_comm _ 0). apply le_n.
+Qed.
+
+Lemma lt_8_9 : 8 < 9.
+Proof.
+  (* unfold lt; apply le_n. *)
+  apply le_n.
+Qed.
+
+SearchPattern (_ + _ <= _)%Z.
+SearchPattern (?X1 * _ <= ?X1 * _)%Z.
+
+Lemma lt_S (n p : nat) : n < p -> n < S p.
+Proof. unfold lt. intro H. apply le_S, H. Qed.
+
+Definition opaque_f : nat -> nat -> nat.
+  intros. assumption.
+Qed.
+
+Lemma bad_proof_example_for_opaque (x y : nat) : opaque_f x y = y.
+Proof.
+  intros.
+  (* unfold opaque_f. (* Error: opaque_f is opaque. *) *)
+Abort.
+
+Open Scope Z_scope.
+
+Definition Zsquare_diff (x y : Z):= x * x - y * y.
+
+Theorem unfold_example (x y : Z)
+   : x*x = y*y
+     -> Zsquare_diff x y * Zsquare_diff (x+y) (x*y) = 0.
+Proof.
+ intros H. unfold Zsquare_diff at 1.
+ rewrite H. ring.
+Qed.
+
