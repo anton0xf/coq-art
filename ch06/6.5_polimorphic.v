539c2181f0803b6d10d46e5e408f0099edcc22af
anton0xf <anton0xf@gmail.com>
2021-08-01 02:05:08 +0300

    ch06: 6.5 * Dependent Inductive Types - exercise 6.47

--- ch06/6.5_polimorphic.v
+++ ch06/6.5_polimorphic.v
@@ -1,4 +1,6 @@
-Require Export ZArith List Arith Bool Lia.
+Require Import ZArith List Arith Bool Lia.
+Import ListNotations.
+
 (* 6.5 * Dependent Inductive Types *)
 
 (* 6.5.1 First-Order Data as Parameters *)
@@ -27,6 +29,9 @@ Inductive htree (A : Type) : nat -> Type
   := hleaf : A -> htree A 0
    | hnode : forall n : nat, A -> htree A n -> htree A n -> htree A (S n).
 
+Arguments hleaf {A}.
+Arguments hnode {A}.
+
 Check htree_ind.
 (* htree_ind : forall (A : Type) (P : forall n : nat, htree A n -> Prop),
      (forall a : A, P 0 (hleaf A a))
@@ -43,15 +48,15 @@ Inductive Z_btree : Set
 
 Fixpoint htree_to_btree (n : nat) (t : htree Z n) {struct t} : Z_btree
   := match t with
-     | hleaf _ x => Z_bnode x Z_leaf Z_leaf
-     | hnode _ p v t1 t2
+     | hleaf x => Z_bnode x Z_leaf Z_leaf
+     | hnode p v t1 t2
        => Z_bnode v (htree_to_btree p t1) (htree_to_btree p t2)
      end.
 
-Fixpoint invert (A : Type) (n : nat) (t : htree A n) {struct t} : htree A n
+Fixpoint invert {A : Type} (n : nat) (t : htree A n) {struct t} : htree A n
   := match t in htree _ x return htree A x with
-     | hleaf _ v => hleaf A v
-     | hnode _ p v t1 t2 => hnode A p v (invert A p t2) (invert A p t1)
+     | hleaf v => hleaf v
+     | hnode p v t1 t2 => hnode p v (invert p t2) (invert p t1)
      end.
 
 (* Exercise 6.46 **
@@ -59,17 +64,17 @@ Fixpoint invert (A : Type) (n : nat) (t : htree A n) {struct t} : htree A n
 Open Scope nat_scope.
 Definition htree_left (X : Type) (n : nat) (t : htree X (S n)) : htree X n
   := match t with
-     | hnode _ n v t1 t2 => t1
+     | hnode n v t1 t2 => t1
      end.
 
 Theorem injection_left_htree (n : nat) (t1 t2 t3 t4 : htree nat n)
-   : hnode nat n O t1 t2 = hnode nat n O t3 t4 ->  t1 = t3.
+   : hnode n O t1 t2 = hnode n O t3 t4 ->  t1 = t3.
 Proof.
   intro H. apply (f_equal (htree_left nat n)) in H. apply H.
 Qed.
 
 (* authors' solution *)
-Definition first_of_htree (A : Type) (n : nat)
+Definition first_of_htree {A : Type} (n : nat)
   : htree A n -> htree A (S n) -> htree A n.
   intros v t. generalize v.
   change (htree A (pred (S n)) -> htree A (pred (S n))).
@@ -89,22 +94,84 @@ Print first_of_htree.
          | hnode _ p _ t1 _ => fun _ : htree A (pred (S p)) => t1
          end) v *)
 
-Check (first_of_htree nat 0 (hleaf nat 1)
-                      (hnode nat 0 2 (hleaf nat 3) (hleaf nat 4))).
-(* first_of_htree nat 0 (hleaf nat 1)
-       (hnode nat 0 2 (hleaf nat 3) (hleaf nat 4))
+Check (first_of_htree 0 (hleaf 1)
+                      (hnode 0 2 (hleaf 3) (hleaf 4))).
+(* first_of_htree 0 (hleaf 1) (hnode 0 2 (hleaf 3) (hleaf 4))
    : htree nat 0 *)
 
-Compute (first_of_htree nat 0 (hleaf nat 1)
-                        (hnode nat 0 2 (hleaf nat 3) (hleaf nat 4))).
-(* = hleaf nat 3 : htree nat 0 *)
+Compute (first_of_htree 0 (hleaf 1)
+                        (hnode 0 2 (hleaf 3) (hleaf 4))).
+(* = hleaf 3 : htree nat 0 *)
 
 Theorem injection_first_htree (n : nat) (t1 t2 t3 t4 : htree nat n)
-  : hnode nat n O t1 t2 = hnode nat n O t3 t4 -> t1 = t3.
+  : hnode n O t1 t2 = hnode n O t3 t4 -> t1 = t3.
 Proof.
   intro H.
   change
-    (first_of_htree nat n t1 (hnode nat n O t1 t2)
-     = first_of_htree nat n t1 (hnode nat n O t3 t4)).
+    (first_of_htree n t1 (hnode n O t1 t2)
+     = first_of_htree n t1 (hnode n O t3 t4)).
   now rewrite H.
 Qed.
+
+(* Exercise 6.47
+   Define a function that takes a number n and builds
+   a fixedheight tree of height n containing
+   pairwise different -integer- natural values
+   (proofs with [nat] is slightly simpler). *)
+
+(* [pn_tree] is for "perfect numbered tree" *)
+
+(* Helper function for [pn_tree]
+   * h - height of tree
+   * n - value of root of tree
+   Nodes numbered like its arryay indexes in ahnentafel tree representaton.
+   See [Binary heap implementation](https://en.wikipedia.org/wiki/Binary_heap#Heap_implementation)
+ *)
+Fixpoint pn_tree_aux (h n : nat) {struct h} : htree nat h
+  := match h with
+     | O => hleaf n
+     | S h' => hnode h' n
+                    (pn_tree_aux h' (2*n + 1))
+                    (pn_tree_aux h' (2*n + 2))
+     end.
+
+Definition pn_tree (h : nat) : htree nat h := pn_tree_aux h 0.
+
+Example pn_tree_ex2
+  : pn_tree 2 = hnode 1 0
+                      (hnode 0 1 (hleaf 3) (hleaf 4))
+                      (hnode 0 2 (hleaf 5) (hleaf 6)).
+Proof. reflexivity. Qed.
+
+Example pn_tree_ex3
+  : pn_tree 3
+    = hnode 2 0
+            (hnode 1 1
+                   (hnode 0 3 (hleaf 7) (hleaf 8))
+                   (hnode 0 4 (hleaf 9) (hleaf 10)))
+            (hnode 1 2
+                   (hnode 0 5 (hleaf 11) (hleaf 12))
+                   (hnode 0 6 (hleaf 13) (hleaf 14))).
+Proof. reflexivity. Qed.
+
+(* authors' solution (with Z replaced by [nat])*)
+Fixpoint make_htree_aux (h start : nat) : nat * htree nat h
+  := match h with
+     | 0 => (start + 1, hleaf start)
+     | S h' => let p := make_htree_aux h' (start + 1) in
+              let q := make_htree_aux h' (fst p) in
+              (fst q, hnode h' start (snd p) (snd q))
+     end.
+
+Definition make_htree (n : nat) : htree nat n := snd (make_htree_aux n 0).
+
+Example make_htree_ex3
+  : make_htree 3
+    = hnode 2 0
+            (hnode 1 1
+                   (hnode 0 2 (hleaf 3) (hleaf 4))
+                   (hnode 0 5 (hleaf 6) (hleaf 7)))
+            (hnode 1 8
+                   (hnode 0 9 (hleaf 10) (hleaf 11))
+                   (hnode 0 12 (hleaf 13) (hleaf 14))).
+Proof. reflexivity. Qed.
