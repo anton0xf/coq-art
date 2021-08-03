b76b95a2ef95f089f225668973bedd937c201d93
anton0xf <anton0xf@gmail.com>
2021-08-03 03:07:53 +0300

    ch06: 6.5 * Dependent Inductive Types, exercise 6.47: prove that pn_tree's nodes are pairwise different

--- ch06/6.5_polimorphic.v
+++ ch06/6.5_polimorphic.v
@@ -296,6 +296,207 @@ Fixpoint htree_is_pwdiff {X : Type} {h : nat} (t : htree X h) {struct t} : Prop
      | hleaf x => True
      | hnode h' x t1 t2
        => htree_is_pwdiff t1 /\ htree_is_pwdiff t2
-         /\ ~ In_htree x t1 /\ ~ In_htree x t2
-         /\ (forall y1 y2 : X, In_htree y1 t1 -> In_htree y2 t1 -> y1 <> y2)
+         /\ (forall y : X, In_htree y t1 -> y <> x)
+         /\ (forall y : X, In_htree y t2 -> y <> x)
+         /\ (forall y1 y2 : X, In_htree y1 t1 -> In_htree y2 t2 -> y1 <> y2)
      end.
+
+Theorem not_in_tree_iff {X : Type} (x : X) (h : nat) (t : htree X h)
+  : (forall y : X, In_htree y t -> y <> x) <-> ~ In_htree x t.
+Proof.
+  split; intro H.
+  - intro H1. apply H in H1. apply H1. reflexivity.
+  - intros y H1 eq. subst y. apply H, H1.
+Qed.
+
+Theorem not_in_pn_tree_aux (h n k : nat)
+  : n < k -> ~ In_htree n (pn_tree_aux h k).
+Proof.
+  generalize dependent k.
+  induction h as [| h1 IH]; simpl; try lia.
+  intros k k_gt [H | [H | H]]; try lia.
+  - apply (IH (k + (k + 0) + 1)); [lia | apply H].
+  - apply (IH (k + (k + 0) + 2)); [lia | apply H].
+Qed.
+
+Theorem pn_tree_contains_left_branch (h n y : nat)
+  : In_htree y (pn_tree_aux h (2 * n + 1))
+    -> In_htree y (pn_tree_aux (S h) n).
+Proof.
+  intro H. simpl. right. left. exact H.
+Qed.
+
+Theorem pn_tree_contains_right_branch (h n y : nat)
+  : In_htree y (pn_tree_aux h (2 * n + 2))
+    -> In_htree y (pn_tree_aux (S h) n).
+Proof.
+  intro H. simpl. right. right. exact H.
+Qed.
+
+Theorem pn_tree_max (h n y : nat)
+  : In_htree y (pn_tree_aux h n) -> y <= 2^h * (n + 2) - 2.
+Proof.
+  generalize dependent n.
+  induction h as [| h1 IH]; intros n H.
+  - simpl in *. lia.
+  - destruct h1 as [| h2];
+      [|remember (S h2) as h1 eqn:eq_h1];
+      simpl in H;
+      replace (n + (n + 0)) with (2*n) in H by lia;
+      destruct H as [H | [H | H]];
+      try (subst y; simpl; lia).
+    + subst n. rewrite Nat.pow_succ_r'.
+      rewrite <- Nat.mul_assoc.
+      rewrite <- Nat.mul_pred_r.
+      rewrite <- Nat.double_twice. unfold Nat.double.
+      apply le_plus_trans, Nat.lt_le_pred.
+      rewrite <- (Nat.mul_1_l y) at 1.
+      apply Nat.mul_lt_mono; [|lia].
+      apply Nat.pow_gt_1; lia.
+    + apply IH in H. simpl in *. lia.
+    + apply IH in H. simpl in *. lia.
+Qed.
+
+Theorem pn_tree_in_Sh' (h n y : nat)
+  : In_htree y (pn_tree_aux (S h) n)
+    -> In_htree y (pn_tree_aux h n)
+      \/ (2^(S h) * (n + 1) - 1) <= y.
+Proof.
+  generalize dependent n.
+  induction h as [| h' IH]; intros n H.
+  - simpl in *. lia.
+  - remember (S h') as h eqn:eq_h. simpl in H.
+    replace (n + (n + 0)) with (2*n) in H by lia.
+    destruct H as [H | [H | H]].
+    + left. subst h n. simpl. left. reflexivity.
+    + apply IH in H. destruct H as [H | H].
+      * left. subst h. apply pn_tree_contains_left_branch, H.
+      * subst h. right. simpl in *. lia.
+    + apply IH in H. destruct H as [H | H].
+      * left. subst h. apply pn_tree_contains_right_branch, H.
+      * subst h. right. simpl in *. lia.
+Qed.
+
+Theorem pn_tree_in_Sh (h n y : nat)
+  : In_htree y (pn_tree_aux (S h) n)
+    -> In_htree y (pn_tree_aux h n)
+      \/ (2^(S h) * (n + 1) - 1) <= y <= 2^(S h) * (n + 2) - 2.
+Proof.
+  intro H. apply pn_tree_in_Sh' in H as H'.
+  destruct H' as [H' | H'].
+  - left. assumption.
+  - right. split.
+    + assumption.
+    + apply pn_tree_max in H. lia.
+Qed.
+
+Theorem pow_ge_1 (n m : nat) : 1 <= n -> 1 <= n ^ m.
+Proof.
+  intro H. induction m as [| m' IH].
+  - simpl. apply Nat.le_refl.
+  - simpl. rewrite <- (Nat.mul_1_l 1).
+    apply Nat.mul_le_mono; lia.
+Qed.
+
+Lemma le_sub (a b c : nat) : b <= a -> c <= a -> a - b <= a - c -> c <= b.
+Proof.
+  intros H1 H2 H.
+  apply (Nat.le_sub_le_add_r _ _ b) in H.
+  rewrite <- Nat.add_sub_swap in H; [|exact H2].
+  apply (plus_le_compat_r _ _ c) in H.
+  rewrite <- Nat.add_sub_swap in H; [|lia].
+  rewrite Nat.add_sub in H.
+  apply (Nat.add_le_mono_l c b a), H.
+Qed.
+
+Lemma le_sub2 (a b c d : nat)
+  : b <= a -> d <= c -> a - b <= c - d -> a + d <= c + b.
+Proof.
+  intros H1 H2 H.
+  apply (Nat.le_sub_le_add_r _ _ b) in H.
+  rewrite <- Nat.add_sub_swap in H; [|exact H2].
+  apply (plus_le_compat_r _ _ d) in H.
+  rewrite <- Nat.add_sub_swap in H; [|lia].
+  rewrite Nat.add_sub in H. exact H.
+Qed.
+
+Theorem pn_tree_last_layer (h n y : nat)
+  : (2^h * (n + 1) - 1) <= y <= 2^h * (n + 2) - 2
+    -> In_htree y (pn_tree_aux h n).
+Proof.
+  generalize dependent n.
+  induction h as [| h1 IH]; intros n [y_gt y_lt].
+  - simpl in *. lia.
+  - pose (IH (2*n + 1)) as H1.
+    pose (IH (2*n + 2)) as H2.
+    destruct (y <=? 2 ^ h1 * (2 * n + 1 + 2) - 2) eqn:neq.
+    + apply Nat.leb_le in neq.
+      apply pn_tree_contains_left_branch, H1.
+      simpl in y_gt. split; lia.
+    + apply Nat.leb_gt in neq. unfold lt in neq.
+      rewrite <- Nat.add_1_r in neq.
+      rewrite <- Nat.add_sub_swap in neq.
+      2:{ rewrite <- (Nat.mul_1_l 2) at 1.
+          apply Nat.mul_le_mono; [|lia].
+          apply pow_ge_1. lia. }
+      apply pn_tree_contains_right_branch, H2.
+      simpl in y_lt. split; lia.
+Qed.
+
+Theorem pow_ge (a b c : nat) : 0 < a <= c -> a <= a ^ b * c.
+Proof.
+  intros [a_gt a_lt]. rewrite <- (Nat.mul_1_l a) at 1.
+  apply Nat.mul_le_mono; [|lia].
+  apply pow_ge_1. lia.
+Qed.
+
+Theorem pn_tree_branches_is_diff (h n y : nat)
+  : In_htree y (pn_tree_aux h (2*n + 1))
+    -> ~ In_htree y (pn_tree_aux h (2*n + 2)).
+Proof.
+  generalize dependent n.
+  induction h as [| h1 IH]; [simpl; lia|].
+  intros n H1 H2.
+  apply pn_tree_in_Sh in H1.
+  apply pn_tree_in_Sh in H2.
+  destruct H1 as [H1 | H1]; destruct H2 as [H2 | H2].
+  - apply (IH n H1 H2).
+  - apply pn_tree_max in H1. destruct H2 as [y_gt y_lt].
+    pose (Nat.le_trans _ y _ y_gt H1) as H.
+    replace (2 * n + 2 + 1) with (2 * n + 1 + 2) in H by lia.
+    rewrite Nat.pow_succ_r' in H.
+    assert (2 <= 2 ^ h1 * (2 * n + 1 + 2)) as Hgt.
+    { apply pow_ge. lia. }
+    apply le_sub2 in H; lia.
+  - apply pn_tree_max in H2. destruct H1 as [y_gt y_lt].
+    pose (Nat.le_trans _ y _ y_gt H2) as H.
+    replace (2 * n + 1 + 1) with (2 * n + 2) in H by lia.
+    rewrite Nat.pow_succ_r' in H.
+    assert (2 <= 2 ^ h1 * (2 * n + 1 + 2)) as Hgt.
+    { apply pow_ge. lia. }
+    apply le_sub2 in H; lia.
+  - destruct H1 as [_ y_lt]. destruct H2 as [y_gt _].
+    pose (Nat.le_trans _ y _ y_gt y_lt) as H.
+    replace (2 * n + 2 + 1) with (2 * n + 1 + 2) in H by lia.
+    rewrite Nat.pow_succ_r' in H.
+    assert (2 <= 2 ^ h1 * (2 * n + 1 + 2)) as Hgt.
+    { apply pow_ge. lia. }
+    apply le_sub2 in H; lia.
+Qed.
+
+Theorem pn_tree_aux_is_pwdiff (h n : nat) : htree_is_pwdiff (pn_tree_aux h n).
+Proof.
+  generalize dependent n.
+  induction h as [| h' IH]; [reflexivity|].
+  intro n. simpl. repeat split; try apply IH.
+  - apply not_in_tree_iff, not_in_pn_tree_aux. lia.
+  - apply not_in_tree_iff, not_in_pn_tree_aux. lia.
+  - intros y1 y2 H1 H2. apply not_eq_sym.
+    generalize dependent H2.
+    generalize dependent y2.
+    rewrite (not_in_tree_iff y1).
+    apply pn_tree_branches_is_diff, H1.
+Qed.
+
+Theorem pn_tree_is_pwdiff (h : nat) : htree_is_pwdiff (pn_tree h).
+Proof. apply pn_tree_aux_is_pwdiff. Qed.
